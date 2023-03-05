79c5b4fa2b8bfcc803bce8c01170455aa93f9f59
Kevin Lindholm <kevin.a.lindholm@gmail.com>
2023-03-06 00:00:55 +0200

    conversions correct for other datatypes

--- src/component/Values.js
+++ src/component/Values.js
@@ -8,23 +8,22 @@ function Values(props) {
         <thead>
             <tr>
             <th>Register</th>
-            <th>Number</th>
             <th>Variable Name</th>
-            <th>Format</th>
             <th>Note</th>
             <th>Converted Data</th>
             </tr>
         </thead>
         <tbody>
-            {props.data.map((data, index, arr)=> 
-                <tr key={data.id}>
-                    <td>{data.id}</td>
-                    <td>{data.number}</td>
-                    <td>{data.name}</td>
-                    <td>{data.format}</td>
-                    <td>{data.Note}</td>
-                    <td>{convert(data, index, arr)}</td>
-                </tr>
+            {props.data.map((data, index, arr)=> {
+                if((index > 0 && data.name !== arr[index-1].name) || data.id === 45 || data.id === 47){
+                return <tr key={data.id}>
+                        <td>{data.id}</td>
+                        <td>{data.name}</td>
+                        <td>{data.note}</td>
+                        <td>{convert(data, index, arr)}</td>
+                    </tr>
+                }
+            }
             )}
         </tbody>
       </table>
--- src/data/Converter.js
+++ src/data/Converter.js
@@ -1,8 +1,103 @@
 export function convert(data, index, arr){
+   /* REAL4 format */
    if(data.format === "REAL4" && arr[index+1] !== undefined && data.name === arr[index+1].name){
-      return parseInt("0x" + arr[index+1].rawData.toString(16) + data.rawData.toString(16), 32)
+      if(data.name === "Current input at AI3" && data.id%2 === 0){
+         return "";
+      }
+      let buffer = new ArrayBuffer(4);
+      let view = new DataView(buffer)
+      view.setUint16(0, data.rawData, true);
+      view.setUint16(2, arr[index+1].rawData, true);
+      return view.getFloat32(0, true).toPrecision(10);
    } 
-   return index;
+   
+   /* LONG format */
+   else if(data.format === "LONG" && arr[index+1] !== undefined && data.name === arr[index+1].name){
+      let buffer = new ArrayBuffer(4);
+      let view = new DataView(buffer)
+      view.setUint16(0, data.rawData, true);
+      view.setUint16(2, arr[index+1].rawData, true);
+      return view.getInt32(0, true);
+   } 
+   
+   /* INTEGER format */
+   else if(data.format === "INTEGER"){
+      return parseInt(data.rawData.toString(16).slice(-2), 16);
+   } 
+
+   /* BCD format Date (using three datapoints)*/
+   else if(data.format === "BCD" && arr[index+1] !== undefined && data.name === arr[index+1].name && arr[index+2] !== undefined && arr[index+2].name === arr[index+1].name){
+      return data.rawData;
+   } 
+
+   /* BCD format */
+   else if(data.format === "BCD"){
+      return data.rawData;
+   } 
+
+   /* BIT format Error handler */
+   else if(data.format === "BIT"){
+      let bitVal = data.rawData.toString(2).split("").reverse().join("");
+      let error = "";
+      for(let i = 0; i < bitVal.length; i++){
+         if(bitVal[i] === "1"){
+            switch(i){
+               case 0:
+                  error += "no received signal\n";
+               break;
+               case 1:
+                  error += "low received signal\n";
+               break;
+               case 2:
+                  error += "poor received signal\n";
+               break;
+               case 3:
+                  error += "pipe empty\n";
+               break;
+               case 4:
+                  error += "hardware failure\n";
+               break;
+               case 5:
+                  error += "receiving circuits gain in adjusting\n";
+               break;
+               case 6:
+                  error += "frequency at the frequency output over flow\n";
+               break;
+               case 7:
+                  error += "current at 4-20mA over flow\n";
+               break;
+               case 8:
+                  error += "RAM check-sum error\n";
+               break;
+               case 9:
+                  error += "main clock or timer clock error\n";
+               break;
+               case 10:
+                  error += "parameters check-sum error\n";
+               break;
+               case 11:
+                  error += "ROM check-sum error\n";
+               break;
+               case 12:
+                  error += "temperature circuits error\n";
+               break;
+               case 13:
+                  error += "reserved\n";
+               break;
+               case 14:
+                  error += "internal timer over flow\n";
+               break;
+               default:
+                  error += "analog input over range\n";
+            }
+         }
+      }
+      return error;
+   }
+   
+
+   //BCD for 3 key
+   //BCD for 1 key
 }
 
 /*
