68e909e7e20b41b2a6c406504567b8acffcfde71
Kevin Lindholm <kevin.a.lindholm@gmail.com>
2023-03-06 22:09:23 +0200

    changed fixed height to min-height for accessability

--- src/component/Values.js
+++ src/component/Values.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { convert } from '../data/Converter'
 
 function Values(props) {
-
   return (
         <div className='values'>
             {props.data.map((data, index, arr)=> {
-                if((index+1 < arr.length && data.name !== arr[index+1].name) || data.id === 45 || data.id === 47 || data.id === 99){
+                if((index > 0 && data.name !== arr[index-1].name) || data.id === 45 || data.id === 47 || data.id === 1){
                 return <div key={data.id} className='individual-value'>
-                    <span className='register-num'>Register: {data.number === 1 ? <span>{data.id}</span> : data.id === 99 ? <span>{data.id}-{data.id+1}</span> : data.number === 2 ? <span>{data.id-1}-{data.id}</span> : <span>{data.id-2}-{data.id}</span>}</span>
+                    <span className='register-num'>Register: {data.number === 1 ? <span>{data.id}</span> : data.number === 2 ? <span>{data.id}-{data.id+1}</span> : <span>{data.id}-{data.id+2}</span>}</span>
                         <h2>{data.name}</h2>
                         <span className='converted-data'>{convert(data, index, arr)}</span>
                         <span className='data-units'>{data.note}</span>
